534572a53ffcdd57b7647e97c68b4b97944f9016
Manji <manjicoder@gmail.com>
2021-11-19 15:36:14 +0530

    2nd commit #24 Fetching API Key from News API

--- src/Components/NewItems.js
+++ src/Components/NewItems.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class NewItems extends Component {
-    render() {
-        return (
-            <div>
-                This is NewItems Component
-            </div>
-        )
-    }
+  static propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+  };
+
+  render() {
+    let { title, description } = this.props;
+    return (
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://cdn.24.co.za/files/Cms/General/d/9970/67e44818862b4ecba66127ee2c037915.jpg"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="newsItems" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default NewItems
+export default NewItems;
