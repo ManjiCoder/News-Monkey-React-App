3ca62a1efd49381480f9fba0398588a2e7defffe
Manji <manjicoder@gmail.com>
2022-02-22 01:50:59 +0530

    #30 Fetching News category wise in NewsMonkey React App

--- src/App.js
+++ src/App.js
@@ -4,19 +4,90 @@ import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 export default class App extends Component {
-  country = "us"          //  THESE ARE REQUIED IN API_KEY
-  category = "general"     //  THESE ARE REQUIED IN API_KEY
+  country = "in"          //  THESE ARE REQUIED IN API_KEY
+  // category = "general"     //  THESE ARE REQUIED IN API_KEY
   render() {
     return (
-      <>
-        <Navbar title="NewsMonkey" />
-        <News
-          title="NewsMonkey"
-          pagesize={12}
-          apikey={`https://newsapi.org/v2/top-headlines?country=${this.country}&category=${this.category}&apiKey=ec7735c4db74410f90ffeffaaa8bd570`}
-        />
-      </>
+      <div>
+        <Router>
+          <Navbar title="NewsMonkey" />
+
+          <Routes>
+            <Route path='/' element={
+              <News key={"general"}
+                title="NewsMonkey"
+                pagesize={12}
+                apikey={`https://newsapi.org/v2/top-headlines?country=${this.country}&category=general&apiKey=ec7735c4db74410f90ffeffaaa8bd570`}
+              />
+            } />
+
+            <Route path='/general' element={
+              <News key={"general"}
+                title="NewsMonkey"
+                pagesize={12}
+                apikey={`https://newsapi.org/v2/top-headlines?country=${this.country}&category=general&apiKey=ec7735c4db74410f90ffeffaaa8bd570`}
+              />
+            } />
+
+            <Route path='/business' element={
+              <News key={"business"}
+                title="NewsMonkey"
+                pagesize={12}
+                apikey={`https://newsapi.org/v2/top-headlines?country=${this.country}&category=business&apiKey=ec7735c4db74410f90ffeffaaa8bd570`}
+              />
+            } />
+
+            <Route path='/entertainment' element={
+              <News key={"entertainment"}
+                title="NewsMonkey"
+                pagesize={12}
+                apikey={`https://newsapi.org/v2/top-headlines?country=${this.country}&category=entertainment&apiKey=ec7735c4db74410f90ffeffaaa8bd570`}
+              />
+            } />
+
+            <Route path='/health' element={
+              <News key={"health"}
+                title="NewsMonkey"
+                pagesize={12}
+                apikey={`https://newsapi.org/v2/top-headlines?country=${this.country}&category=health&apiKey=ec7735c4db74410f90ffeffaaa8bd570`}
+              />
+            } />
+
+            <Route path='/science' element={
+              <News key={"science"}
+                title="NewsMonkey"
+                pagesize={12}
+                apikey={`https://newsapi.org/v2/top-headlines?country=${this.country}&category=science&apiKey=ec7735c4db74410f90ffeffaaa8bd570`}
+              />
+            } />
+
+            <Route path='/sports' element={
+              <News key={"sports"}
+                title="NewsMonkey"
+                pagesize={12}
+                apikey={`https://newsapi.org/v2/top-headlines?country=${this.country}&category=sports&apiKey=ec7735c4db74410f90ffeffaaa8bd570`}
+              />
+            } />
+
+            <Route path='/technology' element={
+              <News key={"technology"}
+                title="NewsMonkey"
+                pagesize={12}
+                apikey={`https://newsapi.org/v2/top-headlines?country=${this.country}&category=technology&apiKey=ec7735c4db74410f90ffeffaaa8bd570`}
+              />
+            } />
+
+          </Routes>
+
+        </Router>
+      </div>
     )
   }
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,7 @@
-
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import {Link} from "react-router-dom";
+
 
 export default class Navbar extends Component {
   static defaultProps = {
@@ -14,21 +15,20 @@ export default class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">{this.props.title}</a>
+          <Link className="navbar-brand" to="/">{this.props.title}</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-capitalize">
-              <li className="nav-item"><a className="nav-link" aria-current="page" href="/">Home</a></li>
-              <li className="nav-item"><a className="nav-link" href="/about">about</a></li>
-              <li className="nav-item"><a className="nav-link" href="/business">business</a></li>
-              <li className="nav-item"><a className="nav-link" href="/entertainment">entertainment</a></li>
-              <li className="nav-item"><a className="nav-link" href="/general">general</a></li>
-              <li className="nav-item"><a className="nav-link" href="/health">health</a></li>
-              <li className="nav-item"><a className="nav-link" href="/science">science</a></li>
-              <li className="nav-item"><a className="nav-link" href="/sports">sports</a></li>
-              <li className="nav-item"><a className="nav-link" href="/technology">technology</a></li>
+              <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/business">business</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/entertainment">entertainment</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/general">general</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/health">health</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/science">science</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/sports">sports</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/technology">technology</Link></li>
             </ul>
           </div>
         </div>
